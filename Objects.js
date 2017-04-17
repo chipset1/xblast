fae541207259d255385ead0debb472e2f948d809
chipset1 <stephenaemblem@gmail.com>
2017-04-16 21:29:55 -0400

    Remove unused var from Timer

--- Objects.js
+++ Objects.js
@@ -950,7 +950,7 @@ function Timer(timerInterval, numberOfCycles) {
         // wait toggle whether the timer should run and return turn
         // immediately or if it should wait 1 cycle before returning turn
         wait = wait || false;
-        var curr = (usesFrames) ? frameCount : millis();
+        var curr = millis();
         if(lastInterval < 0 && wait) lastInterval = curr;
         if(curr-lastInterval >= interval || (lastInterval < 0 && !wait)) {
         // if(curr-lastInterval >= interval) {
